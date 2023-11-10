5909a2ff04a772d64f200d949e40d186d34467de
Ryusuke24 <yunglean_24@mail.ru>
2023-11-10 12:55:47 +0300

    page15 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 
 function App() {
-  const name = "user";
-  const age = "30";
+  const arr = [1, 2, 3];
+  const arr2 = [1, 2, 3, 4, 5];
 
   return (
-    <div>
-      name: {name} <br />
-      age: {age}
-    </div>
+    <>
+      <div>
+        <p>{arr[0]}</p>
+        <p>{arr[1]}</p>
+        <p>{arr[2]}</p>
+      </div>
+      <div>
+        <p>{arr2[0]}</p>
+        <p>{arr2[1]}</p>
+        <p>{arr2[2]}</p>
+        <p>{arr2[3]}</p>
+        <p>{arr2[4]}</p>
+      </div>
+    </>
   );
 }
 
