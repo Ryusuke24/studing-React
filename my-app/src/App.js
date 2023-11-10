10bf4a2b99340d2292d79427050c539704b9b32d
Ryusuke24 <yunglean_24@mail.ru>
2023-11-10 12:51:38 +0300

    page14 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,14 @@
 import React from "react";
 
 function App() {
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
-  );
-
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-  );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
+  const name = "user";
+  const age = "30";
 
   return (
-    <>
-      <input />
-      {block}
-    </>
+    <div>
+      name: {name} <br />
+      age: {age}
+    </div>
   );
 }
 
