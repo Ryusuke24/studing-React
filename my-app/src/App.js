7d7bc32a08d5202c8af8e2c9a673d887907fd81c
Ryusuke24 <yunglean_24@mail.ru>
2023-11-10 13:05:24 +0300

    page 16 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 
 function App() {
-  const arr = [1, 2, 3];
-  const arr2 = [1, 2, 3, 4, 5];
+  const obj = { name: "john", surname: "smit" };
 
   return (
     <>
-      <div>
-        <p>{arr[0]}</p>
-        <p>{arr[1]}</p>
-        <p>{arr[2]}</p>
-      </div>
-      <div>
-        <p>{arr2[0]}</p>
-        <p>{arr2[1]}</p>
-        <p>{arr2[2]}</p>
-        <p>{arr2[3]}</p>
-        <p>{arr2[4]}</p>
-      </div>
+      <p>
+        name: <span>{obj.name}</span>, <br />
+        surname: <span>{obj.surname}</span>
+      </p>
     </>
   );
 }
