513cea87dab4227408097c6e9f9c05bca000952e
Ryusuke24 <yunglean_24@mail.ru>
2023-11-10 16:20:02 +0300

    page23 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,37 +5,42 @@ function App() {
     <ul>
       <li>1</li>
       <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
     </ul>
   );
 
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
+  const table = (
+    <table>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+      </tbody>
+    </table>
   );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
-
   return (
-    <>
-      <input />
-      {block}
-    </>
+    <div>
+      {list} <br /> {table}
+    </div>
   );
 }
 
