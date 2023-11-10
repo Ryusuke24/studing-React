5ab17596b569251e1ef555d2df20dcfaabfeaa81
Ryusuke24 <yunglean_24@mail.ru>
2023-11-10 13:42:29 +0300

    page18-19 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,17 @@
 import React from "react";
 
 function App() {
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
-  );
-
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-  );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
+  const li1 = <li>text1</li>;
+  const li2 = <li>text2</li>;
+  const li3 = <li>text3</li>;
 
   return (
     <>
-      <input />
-      {block}
+      <ul>
+        {li1}
+        {li2}
+        {li3}
+      </ul>
     </>
   );
 }
