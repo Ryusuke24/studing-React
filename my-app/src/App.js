1517a1d00e408da6fad5bac010b8dc86aff44f19
Ryusuke24 <yunglean_24@mail.ru>
2023-11-13 14:50:01 +0300

    page34 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,22 @@
 import React from "react";
 
 function App() {
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
-  );
+  function showMess(name) {
+    alert("hello," + name);
+  }
 
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-  );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
+  function showNum(num) {
+    alert(num);
+  }
 
   return (
-    <>
-      <input />
-      {block}
-    </>
+    <div>
+      <button onClick={() => showMess("Daniel")}>click</button>
+      <button onClick={() => showMess("Sasa")}>click</button>
+      <button onClick={() => showNum(1)}>click 1</button>
+      <button onClick={() => showNum(2)}>click 2</button>
+      <button onClick={() => showNum(3)}>click 3</button>
+    </div>
   );
 }
 
