84b15f27d4fb8f740d7c20318539513bb2b16d86
Ryusuke24 <yunglean_24@mail.ru>
2023-11-13 14:31:02 +0300

    page31 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,15 @@
 import React from "react";
 
 function App() {
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
-  );
-
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-  );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
-
-  return (
-    <>
-      <input />
-      {block}
-    </>
-  );
+  function getDigitSum(num) {
+    let sum = 0;
+    num = num.toString().split("");
+    for (let elem of num) {
+      sum += +elem;
+    }
+    return sum;
+  }
+  return <div>{getDigitSum(12345)}</div>;
 }
 
 export default App;
