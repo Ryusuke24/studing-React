fcd1b1b0c9342d8277f2a9f21839245ee5f98c31
Ryusuke24 <yunglean_24@mail.ru>
2023-11-13 14:54:11 +0300

    page 35 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,14 @@
 import React from "react";
 
 function App() {
-  const list = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-    </ul>
-  );
-
-  const list2 = (
-    <ul>
-      <li>text1</li>
-      <li>text2</li>
-      <li>text3</li>
-    </ul>
-  );
-
-  const block = (
-    <div>
-      {""}
-      {list} <p class="one">paragraf 1</p> <p class="two">paragraf 2</p>
-      <ul>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
-      {list2};
-    </div>
-  );
-
+  function func(event) {
+    console.log(event);
+    console.log(event.target);
+  }
   return (
-    <>
-      <input />
-      {block}
-    </>
+    <div>
+      <button onClick={func}>click</button>
+    </div>
   );
 }
 
