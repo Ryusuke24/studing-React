43c0b6f56acf4d094d4307a5eba37e1538e11017
Ryusuke24 <yunglean_24@mail.ru>
2023-11-13 14:38:13 +0300

    page 33 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 
 function App() {
-  function getDigitSum(num) {
-    let sum = 0;
-    num = num.toString().split("");
-    for (let elem of num) {
-      sum += +elem;
-    }
-    return sum;
+  function showMessage() {
+    alert("hello");
   }
-  return <div>{getDigitSum(12345)}</div>;
+
+  function func1() {
+    alert("1");
+  }
+
+  function func2() {
+    alert("2");
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={showMessage}>show</button>
+        <button onClick={func1}>func1</button>
+        <button onClick={func2}>func2</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
