71acb48b51db3fa0b2e2468fa95ebc3311e070d2
Ryusuke24 <yunglean_24@mail.ru>
2023-11-14 14:42:00 +0300

    page37 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <></>;
+  const arr = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>, <li>5</li>];
+
+  return (
+    <>
+      <ul>{arr}</ul>
+    </>
+  );
 }
 
 export default App;
