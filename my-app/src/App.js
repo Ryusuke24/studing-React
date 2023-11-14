9317f32f80f3343a13c640b2d716326368dfdf31
Ryusuke24 <yunglean_24@mail.ru>
2023-11-14 14:44:14 +0300

    page38 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <></>;
+  let arr = [];
+  for (let i = 0; i < 9; i++) {
+    arr.push(<li>{i + 1}</li>);
+  }
+
+  return (
+    <>
+      <ul>{arr}</ul>
+    </>
+  );
 }
 
 export default App;
