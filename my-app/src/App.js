e3fc8ef7c5bf382a7b2d0753d7a8150dc67c8487
Ryusuke24 <yunglean_24@mail.ru>
2023-11-15 13:50:35 +0300

    page55 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
 import uuid from "react-uuid";
 
@@ -7,7 +7,39 @@ function getId() {
 }
 
 function App() {
-  return <>{getId()}</>;
+  const [count, setCount] = useState(0);
+  const [num, Increment] = useState(0);
+  return (
+    <>
+      <div>
+        <p>{num}</p>
+        <button
+          onClick={() => {
+            Increment(num + 1);
+          }}
+        >
+          Inc
+        </button>
+        <button
+          onClick={() => {
+            Increment(num - 1);
+          }}
+        >
+          Dec
+        </button>
+      </div>
+      <div>
+        <p>{count}</p>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          press
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
