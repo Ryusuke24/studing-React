b56f835b270ea62816c7a66573c36d9326b09da6
Ryusuke24 <yunglean_24@mail.ru>
2023-11-22 12:40:39 +0300

    page72 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -61,8 +61,72 @@ function App() {
     return Math.floor(sum / arr.length);
   }
 
+  const initObj = {
+    prop1: "value1",
+    prop2: "value2",
+    prop3: "value3",
+  };
+
+  const [obj, setObj] = useState(initObj);
+
+  function handleChange2(prop, event) {
+    let copy = Object.assign({}, obj);
+    copy[prop] = event.target.value;
+    setObj(copy);
+    console.log({ ...obj });
+  }
+
+  const initDate = {
+    year: 2025,
+    month: 12,
+    day: 31,
+  };
+
+  const [date, setDate] = useState(initDate);
+
+  function onHandleChange(prop, event) {
+    const copy = Object.assign({}, date);
+    copy[prop] = event.target.value;
+    setDate(copy);
+  }
+
   return (
     <>
+      <div>
+        <input
+          defaultValue={date.year}
+          onChange={event => onHandleChange("year", event)}
+        />
+        <input
+          defaultValue={date.month}
+          onChange={event => onHandleChange("month", event)}
+        />
+        <input
+          defaultValue={date.day}
+          onChange={event => onHandleChange("day", event)}
+        />
+        <p>
+          {date.year}-{date.month}-{date.day}
+        </p>
+      </div>
+      <p>==================================================</p>
+      <div>
+        <input
+          defaultValue={obj.prop1}
+          onChange={event => handleChange2(["prop1"], event)}
+        />
+        <input
+          defaultValue={obj.prop2}
+          onChange={event => handleChange2(["prop2"], event)}
+        />
+        <input
+          defaultValue={obj.prop3}
+          onChange={event => handleChange2(["prop3"], event)}
+        />
+        <p></p>
+        {obj.prop1}-{obj.prop2}-{obj.prop3}
+      </div>
+      <p>==================================================</p>
       <div>
         {inputs}
         <p>{getSrAr(arr)}</p>
