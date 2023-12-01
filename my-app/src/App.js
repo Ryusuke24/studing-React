06a0fc385e92c94c4ed0e9ca34db16341553ff9c
Ryusuke24 <yunglean_24@mail.ru>
2023-12-01 17:08:05 +0300

    useRef v 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { nanoid } from "nanoid";
 import uuid from "react-uuid";
 
@@ -7,7 +7,39 @@ function getId() {
 }
 
 function App() {
-  return <>{getId()}</>;
+  const [state, setState] = useState(0);
+  const ref = useRef(0);
+
+  const [text, setText] = useState("text");
+  const refText = useRef("text");
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setState(state + 1)}>
+          state click : {state}
+        </button>
+        <button onClick={() => (ref.current += 1)}>
+          ref click : {ref.current}
+        </button>
+      </div>
+      <div>
+        <p>{text}</p>
+        <button onClick={() => setText(text + "!")}>press (state)</button>
+      </div>
+      <div>
+        <p>{refText.current}</p>
+        <button
+          onClick={() => {
+            refText.current += "!";
+            console.log(refText.current);
+          }}
+        >
+          press (state)
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
